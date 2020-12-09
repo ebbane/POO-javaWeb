ca58fce3f93425d9d7ec0922265dddfe7b9eb35b
ebbane <ebbanediet@gmailcom>
2020-12-09 17:10:53 +0100

    Ajouts springWeb

---
+++ application/src/main/java/com/ynov/application/application.java
@@ -0,0 +1,13 @@
+package com.ynov.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.application;
+
+@application
+public class SpringBootLiveCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(application.class, args);
+	}
+
+}
