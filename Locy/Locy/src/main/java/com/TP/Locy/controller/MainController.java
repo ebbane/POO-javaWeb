950475a4dc3296ccac111cccd90e55e1cb2474c5
ebbane <ebbanediet@gmailcom>
2020-12-15 14:55:19 +0100

    Add homepage mapping

--- Locy/Locy/src/main/java/com/TP/Locy/controller/MainController.java
+++ Locy/Locy/src/main/java/com/TP/Locy/controller/MainController.java
@@ -1,11 +1,34 @@
 package com.TP.Locy.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import java.util.ArrayList;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @Controller
 public class MainController {
 
+
+    // Injectez (inject) via application.properties.
+    @Value("${welcome.message.bienvenue}")
+    private String message;
+ 
+    @Value("${error.message}")
+    private String errorMessage;
+ 
+    @RequestMapping(value = { "/", "/homepage" })
+    public String homepage() {
+        return "homepage";
+    }
+    
+    @RequestMapping(value = { "/annonce", "/annonceList" })
+    public String annonceList() {
+        return "annonceList";
+    }
+    
+    @RequestMapping(value = { "/add", "/addAnnonce" })
+    public String addAnnonce() {
+        return "addAnnonce";
+    }
 }
